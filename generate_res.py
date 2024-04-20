ae5b91f7f49ede6034a52fc42bf95fae8d67321c
Bohan Zhang <b327zhan@uwaterloo.ca>
2024-04-20 16:30:33 -0400

    Create generate_res.py

---
+++ generate_res.py
@@ -0,0 +1,12 @@
+with open('res.pkl', 'rb') as inp:
+    d = pickle.load(inp)
+print(f"Seach for keyword '{d['keyword']}'")
+print("====================================")
+for key in d:
+    if isinstance(key, tuple):
+        print(key)
+        count = 1
+        for text in d[key]:
+            print(f"({count}) {text}")
+            count += 1
+        print("====================================")

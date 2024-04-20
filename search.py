d57453dce416127c2b8d8b72e36fc3bb1a7bd3f9
Bohan Zhang <b327zhan@uwaterloo.ca>
2024-04-20 15:54:10 -0400

    Update search.py

--- search.py
+++ search.py
@@ -1,11 +1,16 @@
 from docx import Document
+import os
+import pickle
 
-# use , to achieve "and" search
 def search(words, text):
-    found = True
-    for word in words.split(","):
-        found = (word.lower() in text.lower()) and found
-    return found
+    res = []
+    orWords = words.split("|")
+    for orWord in orWords:
+        orFound = True
+        for word in orWord.split(","):
+            orFound = (word.lower() in text.lower()) and orFound
+        res.append(orFound)
+    return any(res)
 
 def search_one_file(file_path, key_word):
     res = []
@@ -16,7 +21,28 @@ def search_one_file(file_path, key_word):
             res.append(text)
     return res
 
-# Example usage
-file_path = "./latest/Rel-15/21_series/21905-f20.docx"
-target_word = "Generation,3G"
-res = search_one_file(file_path, target_word)
+def search_3GPP(key_word):
+    dict = {}
+    dict["keyword"] = key_word
+    root = './3GPP'
+    Rels = [d for d in os.listdir(root) if os.path.isdir(os.path.join(root, d))]
+
+    file_count = 0
+    for R in Rels:
+        Relpath = os.path.join(root, R)
+        Series = [d for d in os.listdir(Relpath) if os.path.isdir(os.path.join(Relpath, d))]
+        for S in Series:
+            Spath = os.path.join(Relpath, S)
+            Files = [f for f in os.listdir(Spath) if f.find('.docx')!=-1]
+            for F in Files:
+                Fpath = os.path.join(Spath, F)
+                try:
+                    result = search_one_file(Fpath, key_word)
+                    if len(result) > 0:
+                        dict[(R, S, F)] = result
+                        file_count += 1
+                except:
+                    print("An exception occurred when handling " + Fpath)
+    dict['filecount'] = file_count
+    with open('res.pkl', 'wb') as outp:
+        pickle.dump(dict, outp, pickle.HIGHEST_PROTOCOL)
